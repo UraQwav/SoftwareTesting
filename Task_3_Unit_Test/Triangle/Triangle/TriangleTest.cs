a21ed59e642426affcd66ad977bb8271f1621c03
ura_qw <ura.qwav@gmail.com>
2019-10-03 02:31:34 +0300

    Add project Triangle

---
+++ Task_3_Unit_Test/Triangle/Triangle/TriangleTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Triangle
+{
+    public class TriangleTest
+    {
+        public static bool IsTriangle(double side_a, double side_b, double side_c)
+        {
+            return (side_a > 0 && side_b > 0 && side_c > 0 && (side_a + side_b >= side_c) && (side_a + side_c >= side_b) && (side_b + side_c >= side_a));
+        }
+        static void Main(string[] args) => Console.WriteLine("Triangle!");
+    }
+}
